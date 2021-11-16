1fae22431f884d04491dd771b9cb3e815da67897
minhbao20052001 <85435173+minhbao20052001@users.noreply.github.com>
2021-11-16 22:31:53 +0700

    v1.0.1
    v1.0.1

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -9,7 +9,7 @@ const User = new Schema({
     password: { type: String, required: true },
     permission: { type: String, default: 'Customer' },
     name: { type: String, maxlength: 100, required: true },
-    photo: { type: String, required: true },
+    phone: { type: String, required: true },
     address: { type: String }
 });
 
