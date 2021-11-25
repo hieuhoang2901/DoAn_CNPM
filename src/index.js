4bc27fd84ff2adf852d6b0c8aad16262df61686e
BuiXuanThong <akirakuma5@gmail.com>
2021-11-25 17:06:05 +0700

    add homepage & search bar

--- src/index.js
+++ src/index.js
@@ -37,3 +37,13 @@ route(app);
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`)
 })
+
+// add them
+const Handlebars = require('handlebars')
+Handlebars.registerHelper('if_eq', function(a, b, opts) {
+  if (a == b) {
+      return opts.fn(this)
+  } else {
+      return opts.inverse(this)
+  }
+});
