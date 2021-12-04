8e9db861c8c67e57d7ea261b71d9f95673272308
minhbao20052001 <85435173+minhbao20052001@users.noreply.github.com>
2021-12-04 21:30:34 +0700

    a
    a

--- src/app/controllers/userManageController.js
+++ src/app/controllers/userManageController.js
@@ -1,7 +1,7 @@
 const User = require('./../models/User');
 const Dish = require('./../models/Dish');
 const Order = require('./../models/Order');
-const { mutiMongoosetoObject,MongoosetoObject }  = require('./../../util/mongoose');
+const { mutiMongoosetoObject, MongoosetoObject } = require('./../../util/mongoose');
 
 exports.getAllUser = async (req, res) => {
     if(req.user.permission !== 'Manager') res.redirect('/');
@@ -10,24 +10,24 @@ exports.getAllUser = async (req, res) => {
     const skip = req.params.page * 5;
     const users = await User.find({}).skip(skip).limit(5);
     const count = await User.find({}).count();
-    res.render('manage/show', {user: req.user, count, users: mutiMongoosetoObject(users), page: Number(req.params.page), length: mutiMongoosetoObject(users).length });
+    res.render('manage/show', { user: req.user, count, users: mutiMongoosetoObject(users), page: Number(req.params.page), length: mutiMongoosetoObject(users).length });
 }
 exports.updateUser = async (req, res) => {
     if(req.user.permission !== 'Manager') res.redirect('/');
-    const users = await User.findOneAndUpdate({ phone: req.params.phone }, req.body);
+    const users = await User.findOneAndUpdate({ phonenumber: req.params.phone }, req.body);
     res.redirect('/manage');
 }
 exports.deleteUser = async (req, res) => {
     if(req.user.permission !== 'Manager') res.redirect('/');
-    const users = await User.findOneAndDelete({ phone: req.params.phone });
+    const users = await User.findOneAndDelete({ phonenumber: req.params.phone });
     res.redirect('/manage');
 }
 exports.detailUser = async (req, res) => {
     if(req.user.permission !== 'Manager') res.redirect('/');
-    const users = await User.findOne({ phone: req.params.phone });
+    const users = await User.findOne({ phonenumber: req.params.phone });
     const order = await Order.findOne({ userID: users._id });
     console.log(order);
-    res.render('manage/detail', {user: req.user, users: MongoosetoObject(users), order: MongoosetoObject(order) });
+    res.render('manage/detail', { user: req.user, users: MongoosetoObject(users), order: MongoosetoObject(order) });
 }
 exports.feedback = async (req, res) => {
     if(req.user.permission !== 'Manager') res.redirect('/');
@@ -36,7 +36,7 @@ exports.feedback = async (req, res) => {
     const skip = req.params.page * 5;
     const feedback = await Order.find({}).skip(skip).limit(5);
     const count = await Order.find({}).count();
-    res.render('manage/feedback', {user: req.user, count, feedback: mutiMongoosetoObject(feedback), page: Number(req.params.page), length: mutiMongoosetoObject(feedback).length });
+    res.render('manage/feedback', { user: req.user, count, feedback: mutiMongoosetoObject(feedback), page: Number(req.params.page), length: mutiMongoosetoObject(feedback).length });
 }
 exports.feedbackDelete = async (req, res) => {
     if(req.user.permission !== 'Manager') res.redirect('/');
@@ -45,5 +45,5 @@ exports.feedbackDelete = async (req, res) => {
 }
 exports.note = async (req, res) => {
     if(req.user.permission !== 'Manager') res.redirect('/');
-    res.render('manage/note',{user: req.user});
+    res.render('manage/note', { user: req.user });
 }
